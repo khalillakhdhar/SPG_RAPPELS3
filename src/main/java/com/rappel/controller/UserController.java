261fbf62130dbadba46e901f45f52f3b13a7a0db
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-02 09:45:51 +0100

    controller

--- src/main/java/com/rappel/controller/UserController.java
+++ src/main/java/com/rappel/controller/UserController.java
@@ -1,5 +1,28 @@
 package com.rappel.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
+@RequestMapping("")
+public class UserController {
+/*
+	@GetMapping()
+public ModelAndView affiche()
+{}
+
+	
+	@PostMapping()
+	
+	public String news(@RequestParam String id)
+	{
+		int identifiant=Integer.parseInt(id);
+		return "";
+		
+	}
+	*/
 }
